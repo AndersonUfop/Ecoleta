d3b08dea2f39ffe3125643c44a2cb8db20acb478
Anderson Fernandes <andersonfernandes@visaojr.com.br>
2020-06-03 15:45:58 -0300

    Backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bem-vindo ao React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
