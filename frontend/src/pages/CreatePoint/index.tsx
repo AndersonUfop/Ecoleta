9b31d6fd6e21b04a8c84570901efeafb28eb25bf
Anderson Fernandes <andersonfernandes@visaojr.com.br>
2020-06-05 13:13:37 -0300

    Mobile aplication

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -210,7 +210,7 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
-                    <Map center={[-19.8090502, -43.1809852]} zoom={15} onClick={handleMapClick}>
+                    <Map center={[-19.453068,-43.1068623]} zoom={15} onClick={handleMapClick}>
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
